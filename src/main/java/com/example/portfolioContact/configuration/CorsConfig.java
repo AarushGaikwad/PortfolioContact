83877c89b10f32845076f4b6a253ed61b58b4f36
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-07-23 22:44:55 +0530

    added CORS config for vercel

---
+++ src/main/java/com/example/portfolioContact/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.portfolioContact.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://portfoliocontact-1.onrender.com")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
