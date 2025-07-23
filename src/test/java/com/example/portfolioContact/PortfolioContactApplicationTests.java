0cc93d9d83118efe1ae253c0cd6f7effc509f534
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-07-23 14:09:42 +0530

    initial commit

---
+++ src/test/java/com/example/portfolioContact/PortfolioContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portfolioContact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
